99c124a599ae4e344a08d0b257fe9c9fe734360c
Roy Carter <roycarter@Roys-MacBook-Pro.local>
2022-09-07 21:09:42 -0400

    set up user pick and reformated answer to exactly = computer pic before setting up win lose function

--- java.js
+++ java.js
@@ -1,17 +1,32 @@
-//function to randomly return Rock Paper or Scissors//
+//function to randomly return Rock Paper or Scissors computer pic//
 
 let nameOne = "Rock";
 let nameTwo = "Paper";
 let nameThree = "Scissors";
 
- let number = Math.floor(Math.random()*10);
- 
-function computerPick(){
-if (number < 3 ) {pick = nameOne}
-else if (number <= 6) {pick = nameTwo} else {pick = nameThree};
+let number = Math.floor(Math.random() * 10);
+
+function computerPick() {
+  if (number <= 3) {
+    pick = nameOne;
+  } else if (number <= 6) {
+    pick = nameTwo;
+  } else {
+    pick = nameThree;
+  }
 }
-computerPick(number)
-console.log(pick)
+computerPick(number);
 
+//user input
 
-  
+var userPick = prompt("Enter your choice of Rock,Paper,Scissors");
+
+//convert userPick to standard format to match computerPick
+
+let firstLetter = userPick.slice(0, 1);
+let restOfWord = userPick.slice(1, 100);
+let start = firstLetter.toUpperCase();
+let end = restOfWord.toLowerCase();
+var userPick = start + end;
+
+console.log(userPick);
