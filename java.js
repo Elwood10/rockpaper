80eb25204b0a7a5721d08d5e64e6678b92a8d741
Roy Carter <roycarter@Roys-MacBook-Pro.local>
2022-09-10 17:40:00 -0400

    working code for one round of RPS before trying to make it loop

--- java.js
+++ java.js
@@ -9,7 +9,7 @@ let number = Math.floor(Math.random() * 10);
 function computerPick() {
   if (number <= 3) {
     pick = nameOne;
-  } else if (number <= 6) {
+  } else if (number > 3 && number <= 6) {
     pick = nameTwo;
   } else {
     pick = nameThree;
@@ -19,14 +19,30 @@ computerPick(number);
 
 //user input
 
-var userPick = prompt("Enter your choice of Rock,Paper,Scissors");
+var choice = prompt("Enter your choice of Rock,Paper,Scissors");
 
 //convert userPick to standard format to match computerPick
 
-let firstLetter = userPick.slice(0, 1);
-let restOfWord = userPick.slice(1, 100);
+let firstLetter = choice.slice(0, 1);
+let restOfWord = choice.slice(1, 100);
 let start = firstLetter.toUpperCase();
 let end = restOfWord.toLowerCase();
 var userPick = start + end;
 
-console.log(userPick);
+// set up win or lose function
+
+function rockPaper() {
+  if (userPick == pick) alert("tie");
+  else if (userPick == "Rock" && pick == "Scissors") alert("You Win!");
+  else if (userPick == "Rock" && pick !== "Scissors") alert("You Lose");
+  else if (userPick == "Paper" && pick == "Rock") alert("You Win");
+  else if (userPick == "Paper" && pick !== "Rock") alert("You Lose");
+  else if (userPick == "Scissors" && pick == "Paper") alert("You Win");
+  else if (userPick == "Scissors" && pick !== "Paper") alert("You Lose");
+  else if ((userPick !== nameOne, nameTwo, nameThree))
+    alert("Check your typing");
+}
+
+rockPaper(userPick, pick);
+
+console.log(pick, number);
