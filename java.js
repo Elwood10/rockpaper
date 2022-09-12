0e21a212d7cc61ac4f370f256455830c2d0b76ac
Roy Carter <roycarter@Roys-MacBook-Pro.local>
2022-09-11 20:54:02 -0400

    reworked to run five times, changed alerts to var to try to get counts on wins

--- java.js
+++ java.js
@@ -1,48 +1,53 @@
-//function to randomly return Rock Paper or Scissors computer pic//
-
-let nameOne = "Rock";
-let nameTwo = "Paper";
-let nameThree = "Scissors";
-
-let number = Math.floor(Math.random() * 10);
-
-function computerPick() {
-  if (number <= 3) {
-    pick = nameOne;
-  } else if (number > 3 && number <= 6) {
-    pick = nameTwo;
-  } else {
-    pick = nameThree;
-  }
-}
-computerPick(number);
-
-//user input
-
-var choice = prompt("Enter your choice of Rock,Paper,Scissors");
-
-//convert userPick to standard format to match computerPick
-
-let firstLetter = choice.slice(0, 1);
-let restOfWord = choice.slice(1, 100);
-let start = firstLetter.toUpperCase();
-let end = restOfWord.toLowerCase();
-var userPick = start + end;
-
-// set up win or lose function
+let pick = "one";
+let userPick = "me";
 
 function rockPaper() {
-  if (userPick == pick) alert("tie");
-  else if (userPick == "Rock" && pick == "Scissors") alert("You Win!");
-  else if (userPick == "Rock" && pick !== "Scissors") alert("You Lose");
-  else if (userPick == "Paper" && pick == "Rock") alert("You Win");
-  else if (userPick == "Paper" && pick !== "Rock") alert("You Lose");
-  else if (userPick == "Scissors" && pick == "Paper") alert("You Win");
-  else if (userPick == "Scissors" && pick !== "Paper") alert("You Lose");
+  let nameOne = "Rock";
+  let nameTwo = "Paper";
+  let nameThree = "Scissors";
+
+  let number = Math.floor(Math.random() * 10);
+
+  function computerPick() {
+    if (number <= 3) {
+      pick = nameOne;
+    } else if (number > 3 && number <= 6) {
+      pick = nameTwo;
+    } else {
+      pick = nameThree;
+    }
+  }
+  computerPick(number);
+
+  //user input
+
+  var choice = prompt("Enter your choice of Rock,Paper,Scissors");
+
+  //convert userPick to standard format to match computerPick
+
+  let firstLetter = choice.slice(0, 1);
+  let restOfWord = choice.slice(1, 100);
+  let start = firstLetter.toUpperCase();
+  let end = restOfWord.toLowerCase();
+  var userPick = start + end;
+
+  var winner;
+  // set up win or lose function
+  if (userPick == pick) winner = "tie";
+  else if (userPick == "Rock" && pick == "Scissors") winner = "You Win!";
+  else if (userPick == "Rock" && pick !== "Scissors") winner = "You Lose";
+  else if (userPick == "Paper" && pick == "Rock") winner = "You Win";
+  else if (userPick == "Paper" && pick !== "Rock") winner = "You Lose";
+  else if (userPick == "Scissors" && pick == "Paper") winner = "You Win";
+  else if (userPick == "Scissors" && pick !== "Paper") winner = "You Lose";
   else if ((userPick !== nameOne, nameTwo, nameThree))
     alert("Check your typing");
+  console.log(winner);
+  //rockPaper();
 }
-
 rockPaper(userPick, pick);
 
-console.log(pick, number);
+//function to run game 5 times
+for (i = 1; i < 5; i++) {
+  rockPaper();
+}
